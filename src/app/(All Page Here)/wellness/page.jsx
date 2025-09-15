500a0ab2e8c50b03aeee2075c48646fa18d41415
rowshonummi <rowshonummichowdhury@gmail.com>
2025-09-16 01:17:11 +0600

    completed the wellness page

--- src/app/(All Page Here)/wellness/page.jsx
+++ src/app/(All Page Here)/wellness/page.jsx
@@ -5,12 +5,30 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <Navbar></Navbar>
-            <div className='pt-30'>
-                Welcome to health and wellness
-                e you can tack your calorioes, bmi and more about our website
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum sit eum, iste voluptatum culpa similique accusantium quo mollitia soluta est.
-                <BmiCalculator></BmiCalculator>
+            <Navbar />
+            <div className="pt-30 text-center px-4">
+                {/* Animated Title */}
+                <h1
+                    className="text-3xl md:text-4xl font-extrabold animate-bounce mt-6"
+                    style={{ color: "var(--color-calm-blue)" }}
+                >
+                   Your Complete Health & Wellness Solution
+                </h1>
+
+                {/* Description */}
+                <p
+                    className="mt-4 text-sm md:text-xl max-w-6xl mx-auto animate-fadeIn text-gray-700"
+                   
+                >
+                    CareHive is a unified digital platform designed to support your overall health and wellness. 
+                    Track key health metrics, manage appointments, and follow personalized wellness programs to 
+                    achieve a healthier, more balanced lifestyle.
+                </p>
+
+                {/* BMI Calculator */}
+                <div className="mt-8">
+                    <BmiCalculator />
+                </div>
             </div>
         </div>
     );
