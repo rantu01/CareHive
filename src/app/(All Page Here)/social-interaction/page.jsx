24c400671938172757f5a5cd948d6452178435a2
rowshonummi <rowshonummichowdhury@gmail.com>
2025-09-22 16:46:42 +0600

    Social health interaction folder created

---
+++ src/app/(All Page Here)/social-interaction/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            This is social health interaction page
+        </div>
+    );
+};
+
+export default page;
