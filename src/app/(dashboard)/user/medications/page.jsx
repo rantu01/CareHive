b6a089339b3572aaeca55d43c89bb38ac35faefe
Dip908 <dipchondo007@gmail.com>
2025-09-22 16:44:34 +0600

    add header on the medication page

---
+++ src/app/(dashboard)/user/medications/page.jsx
@@ -0,0 +1,13 @@
+
+import Medication from '@/app/Component/UserComponent/Medication';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Medication/>
+        </div>
+    );
+};
+
+export default page;
--- src/app/Component/UserComponent/Medication.jsx
+++ src/app/Component/UserComponent/Medication.jsx
@@ -1,9 +1,23 @@
+import { Plus } from "lucide-react";
 
 
 const Medication = () => {
+
     return (
         <div>
-            
+            <header className="flex justify-between items-center ">
+                <div>
+                    <h1 className="text-2xl text-[var(--fourground-color)] font-bold mb-1">Medications</h1>
+                    <p>Manage your medication schedule</p>
+                </div>
+                <button className="bg-[var(--dashboard-blue)] rounded flex items-center gap-4 h-fit p-2 cursor-pointer">
+                    <Plus /> Add Medication
+                </button>
+            </header>
+
+            <main>
+                
+            </main>
         </div>
     );
 };
