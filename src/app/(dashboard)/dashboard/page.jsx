94b625fb6a7cbdc40250dcfb3ee89f5ca67405a8
rantu01 <rantumondal06@gmail.com>
2025-09-22 21:55:09 +0600

    layout issue solve

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -1,16 +1,8 @@
-"use client";
-import React from "react";
-import DashboardLayout from "@/app/Component/Dashboard/DashboardLayout";
-import { useUser } from "../../context/UserContext"; // check path
-
-function Page() {
-  const { user, role, loading } = useUser();
-  console.log(role)
-
-  if (loading) return <p>Loading...</p>;             // waiting for user data
-  if (!user) return <p>Please log in</p>; // role check
-
-  return <DashboardLayout role={role} />;
+export default function DashboardPage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4 text-black">Dashboard Home</h2>
+      <p className="text-gray-600">Welcome to the main dashboard.</p>
+    </div>
+  );
 }
-
-export default Page;
--- src/app/(dashboard)/user/layout.jsx
+++
@@ -1,7 +0,0 @@
-import DashboardLayout from "@/app/Component/Dashboard/DashboardLayout";
-
-
-export default function UserLayout({ children }) {
-  return <DashboardLayout role="user">{children}</DashboardLayout>;
-}
-
