e052a920a112330f0c29a6995fedbef076ae38c3
JulfikarRafi <prorafi1234@gmail.com>
2025-10-05 21:47:02 +0600

    Appointment booking info added to database

--- src/app/api/appointments/route.js
+++ src/app/api/appointments/route.js
@@ -1,5 +1,6 @@
+import clientPromise from "@/app/lib/mongodb";
 import { NextResponse } from "next/server";
-import clientPromise from "../../../lib/mongodb";
+// import clientPromise from "../../../lib/mongodb";
 
 export async function POST(request) {
   try {
