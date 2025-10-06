3b7fead987c0154bffc0d9edcc729ccb15311f25
JulfikarRafi <prorafi1234@gmail.com>
2025-10-06 23:59:12 +0600

    Ui again updated

--- src/app/(public)/(All Page Here)/doctors/page.jsx
+++ src/app/(public)/(All Page Here)/doctors/page.jsx
@@ -212,7 +212,7 @@ export default function DoctorsPage() {
                       fontFamily: "var(--font-primary)",
                     }}
                   >
-                    Details
+                    Details...
                   </button>
                 </div>
 
