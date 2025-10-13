251f4a332aa23262a654542c7a33c693cc9c616f
JulfikarRafi <prorafi1234@gmail.com>
2025-10-13 22:50:35 +0600

    fitness page added

---
+++ src/app/(public)/(All Page Here)/fitness/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='mt-96'>
+            <h1>This is fitness page</h1>
+        </div>
+    );
+};
+
+export default page;
