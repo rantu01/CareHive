c2390ee6ab6c53ddd105ee2c50dd8bc887ba35d5
JulfikarRafi <prorafi1234@gmail.com>
2025-10-14 18:47:26 +0600

    Yoga section completed both frontend and backend

--- src/app/(public)/(All Page Here)/fitness/page.jsx
+++ src/app/(public)/(All Page Here)/fitness/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import YogaTechnique from '../yoga-technique/page';
 
 const page = () => {
     return (
         <div className='mt-96'>
             <h1>This is fitness page</h1>
+            <YogaTechnique></YogaTechnique>
         </div>
     );
 };
