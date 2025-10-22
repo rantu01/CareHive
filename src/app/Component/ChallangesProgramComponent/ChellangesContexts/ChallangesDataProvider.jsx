33127036a27e83f305e8c2f6073dcf64ad36670e
Dip908 <dipchondo007@gmail.com>
2025-10-22 09:37:56 +0600

    fetch challenges data using tanstack query

--- src/app/Component/ChallangesProgramComponent/ChellangesContexts/ChallangesDataProvider.jsx
+++ src/app/Component/ChallangesProgramComponent/ChellangesContexts/ChallangesDataProvider.jsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { ChallangesDataContext } from "./ChallangesDataContext";
+import axios from "axios";
 
 
 const ChallangesDataProvider = ({ children }) => {
@@ -8,6 +10,28 @@ const ChallangesDataProvider = ({ children }) => {
 
     const challanges = "challanges data provider"
 
+    const fetchChallangesData = async () => {
+        try {
+            const response = await axios.get('/api/challenges');
+            console.log(response)
+            return response.data
+        } catch (error) {
+            console.error("Error fetching challanges data:", error);
+            return [];
+        }
+    };
+
+    const {
+        data: challangesData = [],
+        isLoading: isChallangesLoading,
+        error: isChallangesError,
+    } = useQuery({
+        queryKey: ['challangesData'],
+        queryFn: fetchChallangesData
+    })
+
+
+    console.log("the challenges data is",challangesData)
 
     const allChallangesData = {
         challanges
