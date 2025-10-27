fc723326cc45bc0c556018791b9c0f65e4b3ebc6
Dip908 <dipchondo007@gmail.com>
2025-10-28 01:01:35 +0600

    send blog content in backend

--- src/app/api/summarize-blog/route.js
+++ src/app/api/summarize-blog/route.js
@@ -1,17 +1,17 @@
+import { summarizePrompt } from "@/app/utils/blog-summarizer-prompt";
 import { NextResponse } from "next/server"
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.NEXT_STRIPE_SECRET_KEY)
 
 
 export const POST = async (req) => {
-
+    const { blogDetails } = await req.json()
     try {
-        const data = await req.params
-        console.log(data)
 
-        return NextResponse.json({ success: true })
+        return NextResponse.json({ summarizeText: "aiResponseText" })
     } catch (error) {
-        return NextResponse.json({ "error": error })
+        console.error("Error generating Gemini response:", error.message);
+        return NextResponse.json({ error: "something happen wrong please try again" })
     }
 }
 
---
+++ src/app/utils/blog-summarizer-prompt.js
@@ -0,0 +1,12 @@
+export const summarizePrompt = `You are an expert content summarizer for a blogging platform.
+Your task is to read a blog article and produce a concise, engaging, and neutral summary.
+Focus on the main ideas and arguments, accurately reflecting the author’s intent without unnecessary details or repetition.
+If the text contains opinions, summarize them fairly and objectively.
+
+Output Requirements:
+
+Length: 3–5 sentences
+
+Tone: Neutral and professional
+
+Avoid: Direct quotes, redundancy, or personal commentary.Here is the Original Blog Content:`
